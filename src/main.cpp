a2b73411871805cf1161bf176c795dc10679d6f9
sikny <pamartqu@gmail.com>
2022-04-14 16:18:37 +0200

    dataset copied on main project

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,15 @@
 #include <iostream>
 
 int main(int argc, char ** argv){
-    std::cout << "Hello world" << std::endl;
+    /*
+     * TODO
+     * read cards atlas
+     * train two models
+     * exec prog 1 & 2 with image
+     * find card in selected rectangle
+     * compare results of prog 1 & 2
+     * compare execution time of prog 1 & 2
+     * */
 
     return 0;
 }
