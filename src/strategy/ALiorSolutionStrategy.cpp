ec61a29cf05a90d6b7e42f26920d7747b4f83779
Quentin <pamartqu@gmail.com>
2022-04-15 19:06:42 +0200

    fix solutionstrategy compare

--- src/strategy/ALiorSolutionStrategy.cpp
+++ src/strategy/ALiorSolutionStrategy.cpp
@@ -1,34 +1,41 @@
 #include "../../include/strategy/ALiorSolutionStrategy.h"
 
-CompareData ALiorSolutionStrategy::compare(TableMatches expected, SolutionAResult output) const
-{
-	// TD : Comparer expected et output
+CompareData ALiorSolutionStrategy::compare(TableMatches expected, SolutionAResult output) const {
+    // TD : Comparer expected et output
 
     int expectedCount = expected.cards.size();
 
-    int foundCount = 0;
-    for(int i = 0; i < expectedCount; ++i){
-        auto currentCard = expected.cards[i];
+    CompareData result{};
 
-        auto found = std::find_if(output.cardsInImage.begin(), output.cardsInImage.end(), [currentCard] (const PokerCard& c) -> bool {
-            return (int) c.getType() == (int) currentCard.cardType && (int) c.getValue() == (int) currentCard.cardValue;
-        });
-        if(found != std::end(output.cardsInImage)){
+    for (int i = 0; i < expectedCount; ++i) {
+        auto currentCard = expected.cards[i];
+        float matchRate = 0;
+
+        auto found = std::find_if(output.cardsInImage.begin(), output.cardsInImage.end(),
+                                  [currentCard](const PokerCard &c) -> bool {
+                                      return (int) c.getType() == (int) currentCard.cardType &&
+                                             (int) c.getValue() == (int) currentCard.cardValue;
+                                  });
+        if (found != std::end(output.cardsInImage)) {
             // found, check distance
 
-            cv::Point2f centerExpected = (currentCard.pos1 + currentCard.pos2 + currentCard.pos3 + currentCard.pos4) / 4;
+            cv::Point2f centerExpected =
+                    (currentCard.pos1 + currentCard.pos2 + currentCard.pos3 + currentCard.pos4) / 4;
             auto outputEdges = found->getImageEdges();
             cv::Point2f centerOutput = (outputEdges[0] + outputEdges[1] + outputEdges[2] + outputEdges[3]) / 4;
             double distance = norm(centerOutput - centerExpected);
-            if(distance < TOLERANCE_CENTER_DISTANCE){
-                ++foundCount;
+            if (distance < TOLERANCE_CENTER_DISTANCE) {
+                if (distance == 0) matchRate = 1;
+                else {
+                    matchRate = TOLERANCE_CENTER_DISTANCE / distance;
+                    matchRate = matchRate > 1 ? 1 : matchRate;
+                }
             }
         }
+        CardData cardData(currentCard.cardValue, currentCard.cardType, matchRate);
+        result.existingCards.push_back(cardData);
     }
 
     // todo faux positifs
-
-    CompareData result{};
-    result.matchRate = (float) foundCount / (float) expectedCount;
-	return result;
+    return result;
 }
