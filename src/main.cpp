499c3b3e01d78c1d210e8a516488320bde40de58
Quentin <pamartqu@gmail.com>
2022-04-21 00:47:24 +0200

    wip similarity map

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,63 @@ BenchmarkData benchmark(
 {
 	std::vector<SolutionData> solutions;
 
+    std::cout << "Reading cards atlas..." << std::endl;
+    cv::Mat cardsAtlas = cv::imread("resources/cards.jpg");
+
+    // building atlas similarities
+    std::map<std::string, std::map<std::string, double>> similarityMap;
+    {
+        const std::vector<int> cardTypes = {
+            14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2
+        };
+        const std::vector<std::string> colors = {
+            "Clubs", "Spades", "Diamonds", "Hearts"
+        };
+
+        int width = cardsAtlas.cols / 4;
+        int height = cardsAtlas.rows / 13;
+
+        cv::Mat card;
+        for(int i = 0; i < 4; ++i){
+            for(int j = 0; j < 13; ++j){
+                card = cardsAtlas(cv::Rect(i * width + 5, j * height + 5, width - 5, height - 5));
+                cv::Mat tmp;
+                cv::cvtColor(card, tmp, cv::COLOR_BGR2GRAY);
+
+                card = tmp;
+                cv::resize(tmp, card, cv::Size(width * 2, height * 2));
+
+                Card newCard(card, cardTypes[j], colors[i]);
+
+                std::stringstream cardNameStream;
+                cardNameStream << cardTypes[j] << colors[i];
+                std::string cardName = cardNameStream.str();
+
+                cv::Mat hist1Mat, hist2Mat;
+
+                for(int i2 = 0; i2 < 4; ++i2){
+                    for(int j2 = 0; j2 < 13; ++j2){
+                        card = cardsAtlas(cv::Rect(i2 * width + 5, j2 * height + 5, width - 5, height - 5));
+                        cv::cvtColor(card, tmp, cv::COLOR_BGR2GRAY);
+
+                        card = tmp;
+                        cv::resize(tmp, card, cv::Size(width * 2, height * 2));
+                        cardNameStream.str("");
+                        cardNameStream << cardTypes[j2] << colors[i2];
+                        std::string card2Name = cardNameStream.str();
+
+
+                        double matchRate = 1-cv::norm(newCard.data, card, cv::NORM_L2);
+
+                        similarityMap[cardName][card2Name] = matchRate;
+                    }
+                }
+            }
+        }
+    }
+
+    std::cout << "Done, starting benchmark..." << std::endl;
+
 	solutions.reserve(strategies.size());
 
 	for (std::shared_ptr<ISolutionStrategy> strategy : strategies)
