51f51857e5e9f47a55da2e74f7f30edb45808738
Lior <diler.lior@gmail.com>
2022-04-21 21:35:54 +0200

    implemetned card charactersitics serialization and annotation

--- Annotator/src/TableMatches.cpp
+++ Annotator/src/TableMatches.cpp
@@ -9,7 +9,11 @@ TableMatches TableMatches::fromJson(const nlohmann::json& data) {
         CardMatch cardObj;
         cardObj.cardType = (Type) card["type"];
         cardObj.cardValue = (Value) card["value"];
-
+        auto characteristics = card["characteristics"];
+        for(const auto& characteristic : characteristics)
+        {
+            cardObj.characteristics.push_back((Characteristics)characteristic);
+        }
         auto points = card["points"];
         cardObj.pos1 = cv::Point2f(points[0][0], points[0][1]); // 0 -> x ; 1 -> y
         cardObj.pos2 = cv::Point2f(points[1][0], points[1][1]);
@@ -29,17 +33,21 @@ nlohmann::json TableMatches::toJson() const
     auto cardsArray = nlohmann::json::array();
     for(auto& card : cards){
         auto cardObject = nlohmann::json::object();
+        auto characteristicsArray = nlohmann::json::array();
         cardObject["type"] = (ushort) card.cardType;
         cardObject["value"] = (ushort) card.cardValue;
-
         auto pointsArray = nlohmann::json::array({
             {card.pos1.x, card.pos1.y},
             {card.pos2.x, card.pos2.y},
             {card.pos3.x, card.pos3.y},
             {card.pos4.x, card.pos4.y},
         });
+        for(auto characteristic: card.characteristics)
+        {
+            characteristicsArray.push_back(characteristic);
+        }
+        cardObject["characteristics"] = characteristicsArray;
         cardObject["points"] = pointsArray;
-
         cardsArray.push_back(cardObject);
     }
     result["cards"] = cardsArray;
